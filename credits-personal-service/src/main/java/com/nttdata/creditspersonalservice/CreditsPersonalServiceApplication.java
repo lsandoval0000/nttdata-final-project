92cf800dbb0dc3c7f9f4c997c76ac92e39af9d88
Luis Sandoval <lsandoval0000@gmail.com>
2023-02-08 13:10:40 -0500

    First commit

---
+++ credits-personal-service/src/main/java/com/nttdata/creditspersonalservice/CreditsPersonalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.creditspersonalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditsPersonalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditsPersonalServiceApplication.class, args);
+	}
+
+}
---
+++ credits-personal-service/src/test/java/com/nttdata/creditspersonalservice/CreditsPersonalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.creditspersonalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditsPersonalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
